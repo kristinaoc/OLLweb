9cabf9fd5927a878fa83ce5ff6a3808393eef0b8
kristinaoc <44693480+kristinaoc@users.noreply.github.com>
2018-11-26 14:28:32 +1000

    stuff
    miscellaneous

---
+++ resources/js/scripts.js
@@ -0,0 +1,71 @@
+cssVars(); 
+
+
+//open nav
+
+
+$('.nav-prompt').click(function() {
+
+    if ($('.nav-inner').attr('aria-expanded') == 'false') {
+
+        $('.nav-inner').attr('aria-expanded', 'true')
+
+    } else {
+        $('.nav-inner').attr('aria-expanded', 'false');
+    }
+
+
+
+    if ($('.nav-prompt').text() == 'Open Navigation') {
+
+        $('.nav-prompt').text('Close Navigation');
+
+    } else {
+        $('.nav-prompt').text('Open Navigation');
+    }
+
+
+
+
+})
+
+
+
+
+
+// nav active class
+
+
+function activeMenu () {
+    var url = window.location.href;
+
+
+    $('.nav-inner a').filter(function( )  {
+        return this.href == url;
+
+    }).addClass('active');
+}
+
+
+activeMenu(); 
+
+
+
+
+// image gallery
+
+
+function imageGallery()  {
+
+    if (!$('.image-gallery').length) {
+
+        return;
+
+    }
+
+    $('.image-gallery a').simpleLightbox();
+}
+
+
+imageGallery(); 
+
